a31d53dea69cf813c0dca30734ecda49caca423c
Cirs Inso <152234609+Engrcirs@users.noreply.github.com>
2024-03-17 21:12:50 +0800

    Add files via upload

---
+++ Text_Editor/src/com/codecirs/PrintTextEditor.java
@@ -0,0 +1,10 @@
+package com.codecirs;
+
+public class PrintTextEditor extends TextEditor {
+    public PrintTextEditor(String initialText){
+        super(initialText);
+    }
+    public void print(){
+        System.out.println(getText());
+    }
+}
